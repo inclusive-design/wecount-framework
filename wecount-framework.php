fc4e280367e5a0c1c988e7e9a836c076d4e5e776
Ned Zimmerman <ned@bight.dev>
2020-03-10 14:48:53 -0600

    feat: scaffold plugin

---
+++ wecount-framework.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wecount Framework
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wecount-framework
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wecount_Framework
+ */
+
+// Your code starts here.
